3e80608a77d36007ff603e31b388333d899495df
Alexander Neubacher <alexander.neubacher@stud.plus.ac.at>
2024-05-02 10:26:10 +0200

    Initialer Commit

---
+++ src/Alexander.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Random;
+
+public class Alexander {
+    private static final Random RANDOM = new Random(42);
+    private static String url = "jdbc:postgresql://localhost/postgres";
+    private static String user = "postgres";
+    private static String pwd = "1234";
+    private static Connection con;
+
+    public static void main(String[] args) throws SQLException {
+        con = DriverManager.getConnection(url, user, pwd);
+
+        con.close();
+    }
+}
